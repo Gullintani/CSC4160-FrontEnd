085f978bfc8f4d64cca9b8081086edc26340d204
Gullintani <116010168@link.cuhk.edu.cn>
2019-11-28 17:10:26 +0800

    Basic infrastructure done. TODO: 1. Improve loading speed 2. API test and data processing 3. make a better UI and adaptive view

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/content.vue'
 
 export default {
   name: 'app',
@@ -14,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import echarts from "echarts";
+import 'echarts-gl';
+
+Vue.prototype.$echarts = echarts;
 
 Vue.config.productionTip = false
 
