b89adee499eae346a26b23786de60e7646c15e57
Gullintani <116010168@link.cuhk.edu.cn>
2019-11-30 23:09:49 +0800

    Enable different color lines

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="container" class="container" style="height:800px; width:800px">
+  <div>
+    <div id="canvas" style="height:100%; width:100%; position:absolute"></div>
   </div>
 </template>
 
@@ -7,153 +8,173 @@
 export default {
   name: 'HelloWorld',
   data() {
+    //   data:[]
   },
   methods: {
+    getData(){
+
+    },
     drawChart() {
-      // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById("container"));
-      // 指定图表的配置项和数据
-      //var ROOT_PATH = "https://github.com/ecomfe/echarts-examples/find/gh-pages/public/"
-      var data = require('../assets/flight.json')
-      var airports = data.airports.map(function (item) {
-        return {
-            coord: [item[3], item[4]]
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById("canvas"));
+        // 指定图表的配置项和数据
+        var data = require('../assets/flight.json')
+        var airports = data.airports.map(
+            function(item){
+                return {coord: [item[3], item[4]]} //points format
+            }
+        );
+      
+        function getAirportCoord(idx) {
+            return [data.airports[idx][3], data.airports[idx][4]];
         }
-      });
-    function getAirportCoord(idx) {
-        return [data.airports[idx][3], data.airports[idx][4]];
-    }
 
     // Route: [airlineIndex, sourceAirportIndex, destinationAirportIndex]
-    var routesGroupByAirline = {};
-    data.routes.forEach(function (route) {
-        var airline = data.airlines[route[0]];
-        var airlineName = airline[0];
-        if (!routesGroupByAirline[airlineName]) {
-            routesGroupByAirline[airlineName] = [];
-        }
-        routesGroupByAirline[airlineName].push(route);
-    });
-
-    var pointsData = [];
-    data.routes.forEach(function (airline) {
-        pointsData.push(getAirportCoord(airline[1]));
-        pointsData.push(getAirportCoord(airline[2]));
-    });
-
-    var series = data.airlines.map(function (airline) {
-        var airlineName = airline[0];
-        var routes = routesGroupByAirline[airlineName];
-
-        if (!routes) {
-            return null;
-        }
-        return {
-            type: 'lines3D',
-            name: airlineName,
-
-            effect: {
-                show: true,
-                trailWidth: 2,
-                trailLength: 0.15,
-                trailOpacity: 1,
-                trailColor: 'rgb(30, 30, 60)'
-            },
-
-            lineStyle: {
-                width: 1,
-                color: 'rgb(50, 50, 150)',
-                // color: 'rgb(118, 233, 241)',
-                opacity: 0.1
-            },
-            blendMode: 'lighter',
-
-            data: routes.map(function (item) {
-                return [airports[item[1]].coord, airports[item[2]].coord];
-            })
-        };
-    }).filter(function (series) {
-        return !!series;
-    });
-    series.push({
-        type: 'scatter3D',
-        coordinateSystem: 'globe',
-        blendMode: 'lighter',
-        symbolSize: 2,
-        itemStyle: {
-            color: 'rgb(50, 50, 150)',
-            opacity: 0.2
-        },
-        data: pointsData
-    });
-
-    myChart.setOption({
-        legend: {
-            selectedMode: 'single',
-            left: 'left',
-            data: Object.keys(routesGroupByAirline),
-            orient: 'vertical',
-            textStyle: {
-                color: '#fff'
+        var routesGroupByAirline = {};
+        data.routes.forEach(
+            function (route) {
+                var airline = data.airlines[route[0]];
+                var airlineName = airline[0];
+                if(!routesGroupByAirline[airlineName]){routesGroupByAirline[airlineName] = [];}
+                routesGroupByAirline[airlineName].push(route);
             }
-        },
-        globe: {
+        );
 
-            environment: require('../assets/starfield2.jpg'),
-            heightTexture: require('../assets/bathymetry_bw_composite_4k.jpg'),
+        var pointsData = [];
+        data.routes.forEach(function (airline) {
+            pointsData.push(getAirportCoord(airline[1]));
+            pointsData.push(getAirportCoord(airline[2]));
+        });
 
-            displacementScale: 0.1,
-            displacementQuality: 'high',
+        //color settings
+        function AirlineToColour(airline) {
+            if(airline == "American Airlines"){
+                return '#0000CD'
+            }
+            if(airline == "Air China"){
+                return '#DC143C'
+            }
+            return '#7CFC00'
+        }
 
-            baseColor: '#000',
+        var series = data.airlines.map(function (airline) {
+            var airlineName = airline[0];
+            var routes = routesGroupByAirline[airlineName];
+            var AirColor = AirlineToColour(airlineName);
+            if(!routes){
+                return null;
+            }
 
-            shading: 'realistic',
-            realisticMaterial: {
-                roughness: 0.2,
-                metalness: 0
+            return{
+                type: 'lines3D',
+                name: airlineName,
+
+                effect: {
+                        show: true,
+                        trailWidth: 3,
+                        trailLength: 0.15,
+                        trailOpacity: 1,
+                        trailColor: 'rgb(30, 30, 60)'
+                },
+
+                lineStyle: {
+                    width: 1,
+                    color: AirColor,
+                    // color: 'rgb(50, 50, 150)',
+                    // color: 'rgb(118, 233, 241)',
+                    opacity: 0.2
+                },
+                blendMode: 'lighter',
+
+                data: routes.map(function (item) {
+                    return [airports[item[1]].coord, airports[item[2]].coord];
+                })
+        };
+        }).filter(function (series) {
+            return !!series;
+        });
+
+        series.push({
+            type: 'scatter3D',
+            coordinateSystem: 'globe',
+            blendMode: 'lighter',
+            symbolSize: 5,
+            itemStyle: {
+                color: 'rgb(51,153,253)',
+                opacity: 0.2
             },
+            data: pointsData
+        });
 
-            postEffect: {
-                enable: true,
-                depthOfField: {
-                    enable: false,
-                    focalDistance: 150
+        myChart.setOption({
+            legend: {
+                selectedMode: 'single',
+                left: 'left',
+                data: Object.keys(routesGroupByAirline),
+                orient: 'vertical',
+                textStyle: {
+                    color: '#fff'
                 }
             },
-            temporalSuperSampling: {
-                enable: true
-            },
-            light: {
-                ambient: {
-                    intensity: 0
+            globe: {
+                baseTexture: require('../assets/geo.jpg'),
+                environment: require('../assets/starfield2.jpg'),
+                heightTexture: require('../assets/bathymetry_bw_composite_4k.jpg'),
+                // heightTexture: require('../assets/bathymetry_bw_composite_4k.jpg'),
+                
+                displacementScale: 0.2,
+                displacementQuality: 'high',
+                // baseColor: '#000',
+
+                shading: 'realistic',
+                realisticMaterial: {
+                    roughness: 1,
+                    metalness: 0,
+                    // roughness: ROOT_PATH + 'asset/get/s/data-1497599804873-H1SHkG-mZ.jpg',
+                    // metalness: ROOT_PATH + 'asset/get/s/data-1497599800643-BJbHyGWQW.jpg',
+                    // textureTiling: [8, 4]
                 },
-                main: {
-                    intensity: 0.1,
-                    shadow: false
+                postEffect: {
+                    enable: true,
+                    depthOfField: {
+                        enable: false,
+                        focalDistance: 150
+                    }
                 },
-                ambientCubemap: {
-                    texture: require('../assets/pisa.png'),//require('../assets/lake.hdr'),
-                    exposure: 1,
-                    diffuseIntensity: 0.5,
-                    specularIntensity: 2
-                }
+                temporalSuperSampling: {
+                    enable: true
+                },
+                light: {
+                    ambient: {
+                        intensity: 0.1
+                    },
+                    main: {
+                        intensity: 1,
+                        shadow: true
+                    },
+                    ambientCubemap: {
+                        texture: require('../assets/pisa.png'),//require('../assets/lake.hdr'),
+                        exposure: 1,
+                        diffuseIntensity: 0.5,
+                        specularIntensity: 2
+                    }
+                },
+                viewControl: {
+                    autoRotate: false
+                },
+                silent: true
             },
-            viewControl: {
-                autoRotate: false
-            },
-            silent: true
-        },
-        series: series
-    });
-    window.addEventListener('keydown', function () {
-        series.forEach(function (series, idx) {
-            myChart.dispatchAction({
-                type: 'lines3DToggleEffect',
-                seriesIndex: idx
-            });
-        })
-    });
-    }
+            series: series
+        });
+        window.addEventListener('keydown', function () {
+            series.forEach(function (series, idx) {
+                myChart.dispatchAction({
+                    type: 'lines3DToggleEffect',
+                    seriesIndex: idx
+                });
+            })
+        });
+        }
   },
   mounted() {
       this.drawChart()
